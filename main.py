f490def56788de5ae2a39409978a07eafeb6e43a
oguriwork <oguri.ncv@gmail.com>
2025-09-11 18:14:35 +0900

    ad

--- main.py
+++ main.py
@@ -18,18 +18,12 @@ def reserve():
 # 自分用
 def lottry():
     account = I_Account(
-<<<<<<< HEAD
         id=settings.ACCOUNT_ID,
         password=ACCOUNT_PASSWORD,
         name="me"
         )
     add_lottery= add_lottery()
-=======
-        id=settings.ACCOUNT_ID, password=settings.ACCOUNT_PASSWORD, name="me"
-    )
->>>>>>> caf4679860b0e598561114f5e8efcc38d5fabf72
     netaichi.login(account)
-<<<<<<< HEAD
     netaichi.add_lottery(add_lottery)
     lottery_list = netaichi.get_loettry_list()
     status = netaichi.get_status()
@@ -38,9 +32,6 @@ def lottry():
         netaichi.login(account)
         netaichi.summit_lottery(lottery_list,status)
     
-=======
-    add_lottery()
->>>>>>> caf4679860b0e598561114f5e8efcc38d5fabf72
 
 
 if __name__ == "__main__":
